f26cb524c7a6d05e0793e59bebaba0c69458a0e2
pepeleaks <104858775+Fadi002@users.noreply.github.com>
2023-11-26 02:40:05 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
-__VERSION__ = 'V1.0.2'
+__VERSION__ = 'V1.0.3'
 __CHANGELOG_URL__ = 'https://raw.githubusercontent.com/Fadi002/de4py/main/INFO/changelog.json'
 __VERSION_URL__ = 'https://raw.githubusercontent.com/Fadi002/de4py/main/INFO/version'
