385212e22dabda38fe8bc8aee2c33cbe81f30353
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2024-02-01 20:12:03 +0300

    update v1.0.7

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-__VERSION__ = 'V1.0.6'
+__VERSION__ = 'V1.0.7'
 __CHANGELOG_URL__ = 'https://raw.githubusercontent.com/Fadi002/de4py/main/INFO/changelog.json'
 __VERSION_URL__ = 'https://raw.githubusercontent.com/Fadi002/de4py/main/INFO/version'
 __RPC__ = True
