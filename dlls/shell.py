6c43709c981d48ef9d95c9846ee2388e36d47bd7
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2024-05-01 01:39:21 +0300

    Update shell.py

--- dlls/shell.py
+++ dlls/shell.py
@@ -10,8 +10,11 @@ def execute_command(command):
     exit_code = process.wait()
     return exit_code
 
-def inject_shell(pid):
-    exit_code = execute_command(['dlls\\NativeInjector.exe', os.path.abspath("dlls\\pyshell.dll"), pid])
+def inject_shell(pid, bp:None):
+    if bp == None:
+        exit_code = execute_command(['dlls\\NativeInjector.exe', os.path.abspath("dlls\\pyshell.dll"), pid])
+    else:
+        exit_code = execute_command([f'{bp}\\NativeInjector.exe', os.path.abspath(f"{bp}\\pyshell.dll"), pid])
     if exit_code == 0:
         time.sleep(1)
         handle = os.open('\\\\.\\pipe\\de4py', os.O_RDWR)
@@ -19,11 +22,16 @@ def inject_shell(pid):
     else:
         return (None, False)
 
-def stealth_inject_shell(pid):
+def stealth_inject_shell(pid, bp:None):
     file_name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(5,20)))
-    shutil.copy2('dlls\\NativeInjector.exe', f'dlls\\{file_name}.exe')
-    exit_code = execute_command([f'dlls\\{file_name}.exe', 'StealthInjection', os.path.abspath("dlls\\pyshell.dll"), pid])
-    os.remove(f'dlls\\{file_name}.exe')
+    if bp == None:
+        shutil.copy2('dlls\\NativeInjector.exe', f'dlls\\{file_name}.exe')
+        exit_code = execute_command([f'dlls\\{file_name}.exe', 'StealthInjection', os.path.abspath("dlls\\pyshell.dll"), pid])
+        os.remove(f'dlls\\{file_name}.exe')
+    else:
+        shutil.copy2(f'{bp}\\NativeInjector.exe', f'{bp}\\{file_name}.exe')
+        exit_code = execute_command([f'{bp}\\{file_name}.exe', 'StealthInjection', os.path.abspath(f"{bp}\\pyshell.dll"), pid])
+        os.remove(f'{bp}\\{file_name}.exe')
     if exit_code == 0:
         time.sleep(1)
         handle = os.open('\\\\.\\pipe\\de4py', os.O_RDWR)
