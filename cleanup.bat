335316c20a7affe2f3c891c83e495f879b7117bd
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2026-01-10 21:13:37 +0300

    v2.0.0

--- cleanup.bat
+++
@@ -1,4 +0,0 @@
-for /d /r "." %%d in (__pycache__) do if exist "%%d" rd /s /q "%%d"
-for /d /r "." %%d in (logs) do if exist "%%d" rd /s /q "%%d"
-for /d /r "." %%d in (.vs) do if exist "%%d" rd /s /q "%%d"
-pause
