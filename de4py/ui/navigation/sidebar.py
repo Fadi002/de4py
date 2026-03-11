7e66101c30f930e9d2d9da7d0d4fe234b9199780
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2026-03-10 19:54:39 -0700

    small patches

--- de4py/ui/navigation/sidebar.py
+++ de4py/ui/navigation/sidebar.py
@@ -1,3 +1,12 @@
+# de4py
+# Copyright (c) 2026 Fadi002
+#
+# This file is part of the de4py project.
+#
+# Licensed under Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0).
+#
+# See the LICENSE file for details.
+
 import os
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSpacerItem, QSizePolicy, QLabel, QFrame, QGraphicsDropShadowEffect
 from PySide6.QtCore import Signal, QSize, Qt, Property
