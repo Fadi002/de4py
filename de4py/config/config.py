87e32678bd45be6afd1f7af927cb945e7341ad75
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2026-08-12 13:58:33 +0300

    Bump version to 3.3.2 and remove comment-lint workflow

--- de4py/config/config.py
+++ de4py/config/config.py
@@ -57,7 +57,7 @@ class Settings:
     rpc: bool = True
     stealth_title: bool = True
     load_plugins: bool = True
-    build_num: str = "3.2.2.260704542-stable-de4py"
+    build_num: str = "3.3.2.260812416-stable-de4py"
     api_base_url: str = "https://de4py-api.vercel.app"
     api_timeout: int = 700
     poll_interval: float = 2.0
